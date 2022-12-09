eb678e17bce4dfef8f20e823437aaabe1f223c6e
davidpalacin <palacingimenezdavid.fpllefia@gmail.com>
2022-12-09 13:01:40 +0100

    update

--- my-server/db/db.js
+++ my-server/db/db.js
@@ -5,7 +5,6 @@ const sequelize = new Sequelize(
   process.env.MYSQL_DATABASE || config.development.database,
   process.env.MYSQL_USER || config.development.username,
   process.env.MYSQL_PASSWORD || config.development.password,
-  process.env.MYSQL_PORT || config.development.port,
   {
     host: "localhost",
     port: 3307,
