28a96bc46f6143477e13f2167838bfb1169dca30
davidpalacin <palacingimenezdavid.fpllefia@gmail.com>
2022-12-10 11:44:44 +0100

    update

--- my-server/routes/hoteles.js
+++ my-server/routes/hoteles.js
@@ -8,6 +8,5 @@ router.get("/", HotelController.findAll);
 // Get users by id
 router.get("/:id", HotelController.findByPk);
 router.get("/name/:nombre", HotelController.findByName);
-router.get("/price/:price", HotelController.findByPrice);
 
 module.exports = router;
