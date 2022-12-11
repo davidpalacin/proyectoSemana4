99ebde3b7c9224da6aa64bb529263a8dfb09dc0a
davidpalacin <palacingimenezdavid.fpllefia@gmail.com>
2022-12-11 11:34:15 +0100

    update seeders

--- my-server/seeders/20221207114115-Clientes.js
+++ my-server/seeders/20221207114115-Clientes.js
@@ -74,6 +74,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    //  await queryInterface.bulkDelete('People', null, {}); 
+    await queryInterface.bulkDelete('Clientes', null, {});
   },
 };
